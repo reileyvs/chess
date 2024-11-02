b8a90eca3cb3be469f7ea7b63f3d5493822ee0aa
Vince Reiley <ventsley@gmail.com>
2024-11-01 20:40:12 -0600

    added braces to if / else if branches for readability

--- server/src/main/java/dataaccess/Update.java
+++ server/src/main/java/dataaccess/Update.java
@@ -15,10 +15,18 @@ public interface Update {
         try (var ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
             for (var i=0; i < params.length; i++) {
                 var param = params[i];
-                if (param instanceof String p) ps.setString(i+1,p);
-                else if (param instanceof Integer p) ps.setInt(i+1,p);
-                else if (param instanceof ChessGame p) ps.setString(i+1,p.toString());
-                else if (param == null) ps.setNull(i+1,NULL);
+                if (param instanceof String p) {
+                    ps.setString(i + 1, p);
+                }
+                else if (param instanceof Integer p) {
+                    ps.setInt(i + 1, p);
+                }
+                else if (param instanceof ChessGame p) {
+                    ps.setString(i + 1, p.toString());
+                }
+                else if (param == null) {
+                    ps.setNull(i + 1, NULL);
+                }
             }
             ps.executeUpdate();
 
