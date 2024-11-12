c461740e55ddb454a3b72eaa38fcbd466a97cf0f
Vince Reiley <ventsley@gmail.com>
2024-11-12 11:06:42 -0700

    tweaking for bugs

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,9 +1,11 @@
 package ui;
 
 import chess.ChessGame;
+import client.ServerFacade;
 
 public class Client {
     //All the menu logic and creates and sends chessboard to ChessBoard
+
     public void sendChessBoard() {
         chess.ChessGame game = new chess.ChessGame();
         ChessBoard board = new ChessBoard(game.getBoard().getBoard());
