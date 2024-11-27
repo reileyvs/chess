522486b1265d9f030ab58d3407df62e560f839f4
Vince Reiley <ventsley@gmail.com>
2024-11-27 13:48:39 -0700

    created MainNoServer to test multiple clients on the same server

--- client/src/main/java/main/MainNoServer.java
+++ client/src/main/java/main/MainNoServer.java
@@ -1,14 +1,11 @@
 package main;
 
-import exceptions.DataAccessException;
 import server.Server;
 import ui.Client;
-import websocket.WebSocketClient;
 
-public class Main {
+public class MainNoServer {
     public static void main(String[] args) throws Exception {
-        Server server = new Server();
-        int port = server.run(3030);
+        String port = "3030";
         String host = "http://localhost:";
         Client client = new Client(host + port);
         client.initialMenu();
