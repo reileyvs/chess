b7b999eb1feb7e7845338e0e7e489b6443c34c43
Vince Reiley <ventsley@gmail.com>
2024-12-05 16:04:20 -0700

    code readablility

--- client/src/main/java/main/Main.java
+++ client/src/main/java/main/Main.java
@@ -1,16 +1,12 @@
 package main;
 
 import exceptions.DataAccessException;
-import server.Server;
+//import server.Server;
 import ui.Client;
 import websocket.WebSocketClient;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Server server = new Server();
-        int port = server.run(3030);
-        String host = "http://localhost:";
-        Client client = new Client(host + port);
-        client.initialMenu();
+
     }
 }
